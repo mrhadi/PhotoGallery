377c011f6393d16ddf140375c43ad63ca3977844
Hadi Shad <hadi@mrhadi.com>
2024-12-07 17:37:41 +1100

    Loading some sample data.

--- App/App.tsx
+++ App/App.tsx
@@ -1,18 +1,48 @@
-import React, { useEffect } from 'react';
-import { SafeAreaView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, Image } from 'react-native';
 
 import ApiService from './Services/api';
 const apiService = ApiService();
 
+type GIFs = {
+  bitlyGifUrl: string
+  title: string
+}
+
+type GIFItemProps = {
+  title: string
+  url: string
+};
+
+const GIFItem = ({ title, url }: GIFItemProps) => (
+  <Image source={{ uri: url }} style={{ width: 320, height: 200, backgroundColor: 'red', marginBottom: 5 }} />
+);
+
 const App = () => {
+  const [trendingData, setTrendingData] = useState<[GIFs]>([]);
+
   useEffect(() => {
-    apiService.getTrending().then(res => console.log(res))
-  });
+    apiService.getTrending().then(res => {
+      const data = res?.data?.data;
+      console.log(JSON.stringify(data[0].images.fixedWidthStill));
+
+      const filteredData: [GIFs] = data.map(item => ({
+        bitlyGifUrl: item.images.fixedWidthStill.url,
+        title: item.title,
+      }));
+
+      setTrendingData(filteredData);
+    });
+  }, []);
 
   return (
     <SafeAreaView>
+      <FlatList
+        data={trendingData}
+        renderItem={({ item}) => <GIFItem url={item.bitlyGifUrl} title={item.title} /> }
+      />
     </SafeAreaView>
   );
-}
+};
 
 export default App;
--- App/Services/api.ts
+++ App/Services/api.ts
@@ -1,5 +1,6 @@
 import { createContext } from 'react';
 import axios, { AxiosInstance } from 'axios';
+import applyCaseMiddleware from 'axios-case-converter';
 
 import { logConsole, logError } from './LogTracker.ts';
 import ENV from './env';
@@ -7,10 +8,10 @@ import ENV from './env';
 const BASE_URL = ENV.BASE_API_URL;
 
 const ApiService = () => {
-  const client: AxiosInstance = axios.create({
+  const client: AxiosInstance = applyCaseMiddleware(axios.create({
     baseURL: BASE_URL,
     timeout: 15000,
-  });
+  }));
 
   client.interceptors.request.use(
     config => {
@@ -44,7 +45,7 @@ const ApiService = () => {
   );
 
   const getTrending = async () => {
-    const url = `trending?api_key=${ENV.API_KEY}&limit=15`;
+    const url = `trending?apiKey=${ENV.API_KEY}&limit=15`;
     return client.get(url);
   };
 
