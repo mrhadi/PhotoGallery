2d63399a1bf43dcf167b6e732753f35a23dd8da8
Hadi Shad <hadi@mrhadi.com>
2024-12-07 13:23:03 +1100

    Update project settings.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,19 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  extends: [
+    '@react-native',
+  ],
+  rules: {
+    'no-return-await': 0,
+    'react/prop-types': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    indent: ['error', 2, { SwitchCase: 1 }],
+    '@typescript-eslint/camelcase': 0,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
